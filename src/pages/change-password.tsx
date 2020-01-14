6928e22705c249e8b5933ecc5004744788430923
Laplandin <lopatin@ucoz-team.net>
2020-01-14 11:51:50 +0300

    change pass form refactor

--- src/pages/change-password.tsx
+++ src/pages/change-password.tsx
@@ -1,76 +1,13 @@
 import React, { useCallback, useState } from 'react';
 import {
-    FormGroup,
-    Form,
-    Input,
-    InputGroup,
-    Button,
-    Row,
-    Col,
     Container,
 } from 'reactstrap';
+import ChangePasswordForm from '../components/auth/ChangePasswordForm';
 
-const initialFormState = {
-    password: {
-        value: '',
-        validateErrors: {},
-        type: 'password',
-        placeholder: 'Enter your password',
-    },
-    confirm: {
-        value: '',
-        validateErrors: {},
-        type: 'password',
-        placeholder: 'Confirm your password',
-    },
-};
-
-const ChangePasswordForm = () => {
-    const [formData, setFormData] = useState(initialFormState);
-    const [passwordVisibility, setPasswordVisibility] = useState(false);
-    const onChange = useCallback((key) => (event) => {
-        setFormData({
-            ...formData,
-            [key]: {
-                ...formData[key],
-                value: event.target.value,
-            },
-        });
-    }, [formData]);
-    const onSubmit = useCallback(() => {
-        // TODO Submit
-    }, [formData]);
+export default function ChangePasswordView() {
     return (
-        <Form>
-            {Object.entries(formData).map(([prop, data]) => (
-                <Row>
-                    <Col md="12">
-                        <FormGroup>
-                            <InputGroup className="input-group-alternative mb-4">
-                                <Input
-                                    value={data.value}
-                                    type={passwordVisibility ? 'text' : 'password'}
-                                    placeholder={data.placeholder}
-                                    onChange={onChange(prop)}
-                                />
-                            </InputGroup>
-                        </FormGroup>
-                    </Col>
-                </Row>
-            ))}
-            <Row>
-                <Button onClick={onSubmit} color="primary" type="button">
-                    Change password
-                </Button>
-            </Row>
-        </Form>
+        <Container className="container" fluid="lg">
+            <ChangePasswordForm />
+        </Container>
     );
-};
-
-const ChangePasswordView = () => (
-    <Container className="container" fluid="lg">
-        <ChangePasswordForm />
-    </Container>
-);
-
-export default ChangePasswordView;
+}
