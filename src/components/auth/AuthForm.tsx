a3409108e1700751280d015d63ee766fdebffd49
Laplandin <lopatin@ucoz-team.net>
2020-01-25 19:25:44 +0300

    links, sign-up link from login

--- src/components/auth/AuthForm.tsx
+++ src/components/auth/AuthForm.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import * as classNames from 'classnames';
 import { ClassNameType } from 'globals';
+import Link from 'next/link';
 import { translate } from '@i18n/index';
 import LoginPasswordForm from '@components/auth/LoginPasswordForm';
 
@@ -13,7 +14,14 @@ export default function AuthForm({ className }: AuthFormPropsType) {
         <div className={classNames('c-auth-form d-flex flex-column align-items-center justify-content-between', className)}>
             <h1 className="c-auth-form__title font-weight-bold text-center">{translate('AUTHORIZATION')}</h1>
             <LoginPasswordForm className="c-auth-form__form" />
-            <a href="#" className="c-auth-form__forgot-password">{translate('FORGOT_PASSWORD')}</a>
+            <div className="c-auth-form__actions">
+                <Link href="/sign-up">
+                    <a className="c-auth-form__forgot-password">{translate('SIGN_UP')}</a>
+                </Link>
+                <Link href="/forgot-password">
+                    <a className="c-auth-form__forgot-password">{translate('FORGOT_PASSWORD')}</a>
+                </Link>
+            </div>
         </div>
     );
 }
--- src/components/auth/ForgotPasswordCard.tsx
+++ src/components/auth/ForgotPasswordCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as classNames from 'classnames';
+import Link from 'next/link';
 import { ClassNameType } from 'globals';
 import { translate } from '@i18n/index';
 import ForgotPasswordForm from '@components/auth/ForgotPasswordForm';
@@ -13,7 +14,9 @@ export default function AuthForm({ className }: AuthFormPropsType) {
         <div className={classNames('c-auth-form d-flex flex-column align-items-center justify-content-between', className)}>
             <h1 className="c-auth-form__title font-weight-bold text-center">{translate('RESTORE_PASSWORD')}</h1>
             <ForgotPasswordForm className="c-auth-form__forgot-password" />
-            <a href="#" className="c-auth-form__forgot-password">{translate('AUTHORIZATION')}</a>
+            <Link href="/login">
+                <a className="c-auth-form__forgot-password">{translate('AUTHORIZATION')}</a>
+            </Link>
         </div>
     );
 }
